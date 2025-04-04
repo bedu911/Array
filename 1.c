5f99cff264c5f59c765008071a32ddb9463e70dd
Bedanand Sah <bedu9111@gmail.com>
2025-04-04 18:09:05 +0530

    Traversing

---
+++ 1.c
@@ -0,0 +1,32 @@
+//  Traversing a 1D Array in C
+#include<stdio.h>
+//Function To Take Data From User
+void input(int a[],int n) 
+{
+    printf("Enter the Elements of Array\n");
+   for(int i=0;i<n;i++) //TC:O(n)
+   {
+       scanf("%d",&a[i]);
+   }
+}
+//Function To Display The Elements of Array
+void display(int a[],int n)
+{
+    printf("Array Elements\n");
+    for(int i=0;i<n;i++)//TC:O(n)
+    {
+        printf("%d\t",a[i]);
+    }
+}
+int main()
+{
+    int a[100],n;
+    printf("Enter the Size of Array:");
+    scanf("%d",&n);
+    if(n>100){ 
+        return 0;
+    }
+    input(a,n);
+    display(a,n);
+    return 0;
+}
